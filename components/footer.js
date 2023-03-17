a10140d43ba702fa2d6031babe928a03a86b6cf3
Rexpeto <carlosgallardoparra@hotmail.com>
2023-03-17 18:48:48 -0400

    se agrego copyright y estilos

--- components/footer.js
+++ components/footer.js
@@ -1,10 +1,15 @@
 import Navegacion from "./navegacion";
+import styles from "../styles/footer.module.css";
 
 const Footer = () => {
     return (
-        <footer>
-            <div className="contenedor">
+        <footer className={styles.footer}>
+            <div className={`contenedor ${styles.contenido}`}>
                 <Navegacion />
+
+                <p className={styles.copyright}>
+                    Todos los derechos reservados {new Date().getFullYear()}
+                </p>
             </div>
         </footer>
     );
