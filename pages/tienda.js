9048ddd3b01bd3b4ef22c69f99efc392029c66ab
Rexpeto <carlosgallardoparra@hotmail.com>
2023-03-17 18:06:09 -0400

    Página de la tienda

---
+++ pages/tienda.js
@@ -0,0 +1,14 @@
+import Layout from "@/components/layout";
+
+const Tienda = () => {
+    return (
+        <Layout
+            title="Tienda"
+            description="Las mejores guitarras en nuestra tienda oficial"
+        >
+            <h1>Tienda</h1>
+        </Layout>
+    );
+};
+
+export default Tienda;
