2dea87bf399a28f31074cacd4f56b90e9ce23a78
Rexpeto <carlosgallardoparra@hotmail.com>
2023-03-17 18:07:30 -0400

    Componente de footer

---
+++ components/footer.js
@@ -0,0 +1,13 @@
+import Navegacion from "./navegacion";
+
+const Footer = () => {
+    return (
+        <footer>
+            <div className="contenedor">
+                <Navegacion />
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
