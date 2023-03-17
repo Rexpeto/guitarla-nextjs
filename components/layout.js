bca30279c1ff4ed7df9316dad53979052f5c90e0
Rexpeto <carlosgallardoparra@hotmail.com>
2023-03-17 17:13:31 -0400

    Layout principal

---
+++ components/layout.js
@@ -0,0 +1,19 @@
+import Head from "next/head";
+
+const Layout = ({ children, title = "", description = "" }) => {
+    return (
+        <>
+            <Head>
+                <title>{`GuitarLA - ${title}`}</title>
+                <meta
+                    name="description"
+                    content={`GuitarLa - ${description}`}
+                />
+            </Head>
+
+            {children}
+        </>
+    );
+};
+
+export default Layout;
