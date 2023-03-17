76165c7e5697b50badef71dd1712f5d078543f2f
Rexpeto <carlosgallardoparra@hotmail.com>
2023-03-17 18:05:53 -0400

    Página de blog

---
+++ pages/blog.js
@@ -0,0 +1,14 @@
+import Layout from "@/components/layout";
+
+const Blog = () => {
+    return (
+        <Layout
+            title="Blog"
+            description="El blog oficial de las mejores guitarras en GuitarLa"
+        >
+            <h1>Blog</h1>
+        </Layout>
+    );
+};
+
+export default Blog;
