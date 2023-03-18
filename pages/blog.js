219b78f2258419b639d13379b35df838d31c6b49
Rexpeto <carlosgallardoparra@hotmail.com>
2023-03-18 11:23:05 -0400

    Página finalizada

--- pages/blog.js
+++ pages/blog.js
@@ -1,14 +1,31 @@
 import Layout from "@/components/layout";
+import ListadoPosts from "@/components/listado-posts";
 
-const Blog = () => {
+const Blog = ({ blogs }) => {
     return (
         <Layout
             title="Blog"
             description="El blog oficial de las mejores guitarras en GuitarLa"
         >
-            <h1>Blog</h1>
+            <div className="contenedor">
+                <h1 className="heading">Blog</h1>
+                <ListadoPosts blogs={blogs} />
+            </div>
         </Layout>
     );
+};
+
+export const getServerSideProps = async () => {
+    const respuesta = await fetch(
+        `${process.env.API_URL}/posts?populate=imagen`
+    );
+    const { data: blogs } = await respuesta.json();
+
+    return {
+        props: {
+            blogs,
+        },
+    };
 };
 
 export default Blog;
