3d69ea93957d87d878a4b1f1df957a987a921e7d
Rexpeto <carlosgallardoparra@hotmail.com>
2023-03-17 21:13:02 -0400

    Página finalizada

--- pages/nosotros.js
+++ pages/nosotros.js
@@ -1,5 +1,6 @@
-import Link from "next/link";
 import Layout from "@/components/layout";
+import Image from "next/image";
+import styles from "../styles/nosotros.module.css";
 
 const Nosotros = () => {
     return (
@@ -7,8 +8,24 @@ const Nosotros = () => {
             title="Nosotros"
             description="Venta de guitarras de la mejor calidad, sabiendo un poco de nosotros"
         >
-            <h1>Nosotros</h1>
-            <Link href="/">Volver</Link>
+            <main className={`contenedor ${styles.nosotros}`}>
+                <h2 className="heading">Nosotros</h2>
+
+                <div className={styles.contenido}>
+                    <Image src="/img/nosotros.jpg" width={1000} height={800} />
+                    <div>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing
+                            elit. Repellat odit facilis nostrum officia placeat
+                            consequuntur nam error magnam maxime tempora quae,
+                            id dolorem eos est corrupti totam assumenda eligendi
+                            nesciunt deserunt modi libero. Laborum aut, deserunt
+                            cum ad ipsum id tempore nam optio dolore iusto qui
+                            nihil dolorum iste cumque.
+                        </p>
+                    </div>
+                </div>
+            </main>
         </Layout>
     );
 };
