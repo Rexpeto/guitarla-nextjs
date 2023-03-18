4f1e4b5cadc6a187e57b12d30acd0e973d1e7e6e
Rexpeto <carlosgallardoparra@hotmail.com>
2023-03-18 13:00:48 -0400

    Página finalizada

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,52 @@
-import Link from "next/link";
 import Layout from "@/components/layout";
+import ListadoGuitarras from "@/components/listado-guitarras";
+import ListadoPosts from "@/components/listado-posts";
+import Curso from "@/components/curso";
 
-export default function Home() {
+export default function Home({ guitarras, blogs, curso }) {
     return (
         <>
             <Layout
                 title="Inicio"
                 description="Ventas de guitarras de la mejor calidad"
             >
-                <h1>Hola mundo desde Next.js</h1>
-                <Link href="/nosotros">Nosotros</Link>
+                <div className="contenedor">
+                    <h3 className="heading">Nuestra colección</h3>
+                    <ListadoGuitarras guitarras={guitarras} />
+
+                    <Curso curso={curso?.attributes} />
+
+                    <h3 className="heading">Blog</h3>
+                    <ListadoPosts blogs={blogs} />
+                </div>
             </Layout>
         </>
     );
 }
+
+export const getServerSideProps = async () => {
+    const guitarraUrl = `${process.env.API_URL}/guitarras?populate=imagen`;
+    const blogUrl = `${process.env.API_URL}/posts?populate=imagen`;
+    const cursoUrl = `${process.env.API_URL}/curso?populate=imagen`;
+
+    const [resGuitarra, resBlogs, resCurso] = await Promise.all([
+        fetch(guitarraUrl),
+        fetch(blogUrl),
+        fetch(cursoUrl),
+    ]);
+
+    const [{ data: guitarras }, { data: blogs }, { data: curso }] =
+        await Promise.all([
+            resGuitarra.json(),
+            resBlogs.json(),
+            resCurso.json(),
+        ]);
+
+    return {
+        props: {
+            guitarras,
+            blogs,
+            curso,
+        },
+    };
+};
