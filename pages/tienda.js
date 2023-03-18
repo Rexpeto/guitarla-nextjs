8870d920d02878f69f85b4291b212aa9d18af3d0
Rexpeto <carlosgallardoparra@hotmail.com>
2023-03-18 00:51:27 -0400

    Mostrando guitarras

--- pages/tienda.js
+++ pages/tienda.js
@@ -1,14 +1,31 @@
 import Layout from "@/components/layout";
+import ListadoGuitarras from "@/components/listado-guitarras";
 
-const Tienda = () => {
+const Tienda = ({ guitarras }) => {
+    console.log(guitarras);
     return (
         <Layout
             title="Tienda"
             description="Las mejores guitarras en nuestra tienda oficial"
         >
-            <h1>Tienda</h1>
+            <main className="contenedor">
+                <h2 className="heading">Nuestra colección</h2>
+                <ListadoGuitarras guitarras={guitarras} />
+            </main>
         </Layout>
     );
 };
 
+export const getServerSideProps = async () => {
+    const respuesta = await fetch(
+        `${process.env.API_URL}/guitarras?populate=imagen`
+    );
+    const { data: guitarras } = await respuesta.json();
+
+    return {
+        props: {
+            guitarras,
+        },
+    };
+};
 export default Tienda;
