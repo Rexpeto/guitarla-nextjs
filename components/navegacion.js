49e9e6e798056aea8ee196172b6a46b9bc9875bd
Rexpeto <carlosgallardoparra@hotmail.com>
2023-03-17 20:55:15 -0400

    Ruta activa

--- components/navegacion.js
+++ components/navegacion.js
@@ -1,13 +1,33 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 import styles from "../styles/navegacion.module.css";
 
 const Navegacion = () => {
+    const { route } = useRouter();
+
     return (
         <nav className={styles.navegacion}>
-            <Link href="/">Inicio</Link>
-            <Link href="/blog">Blog</Link>
-            <Link href="/tienda">Tienda</Link>
-            <Link href="/nosotros">Nosotros</Link>
+            <Link href="/" className={route === "/" ? styles.active : ""}>
+                Inicio
+            </Link>
+            <Link
+                href="/blog"
+                className={route === "/blog" ? styles.active : ""}
+            >
+                Blog
+            </Link>
+            <Link
+                href="/tienda"
+                className={route === "/tienda" ? styles.active : ""}
+            >
+                Tienda
+            </Link>
+            <Link
+                href="/nosotros"
+                className={route === "/nosotros" ? styles.active : ""}
+            >
+                Nosotros
+            </Link>
         </nav>
     );
 };
