cca5f148bc8273c5e4af0225a0f3047d06b9c010
Rexpeto <carlosgallardoparra@hotmail.com>
2023-03-18 00:59:16 -0400

    Página 404

---
+++ pages/404.js
@@ -0,0 +1,14 @@
+import Layout from "@/components/layout";
+
+const Pagina404 = () => {
+    return (
+        <Layout
+            title="Página no encontrada"
+            description="Página oficial de las mejores guitarras"
+        >
+            <h1>404</h1>
+        </Layout>
+    );
+};
+
+export default Pagina404;
