2f900f60be5e70faaec974b42e2b49fcc2ed7e79
Rexpeto <carlosgallardoparra@hotmail.com>
2023-03-19 18:52:36 -0400

    Context del carrito

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,56 @@
-import '@/styles/globals.css'
+import { useState, useEffect } from "react";
+import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    const [carrito, setCarrito] = useState([]);
+
+    useEffect(() => {
+        localStorage.setItem("carrito", JSON.stringify(carrito));
+    }, [carrito]);
+
+    const agregarCarrito = (guitarra) => {
+        if (carrito.some((guitarraState) => guitarraState.id === guitarra.id)) {
+            const carritoActualizado = carrito.map((guitarraState) => {
+                if (guitarraState.id === guitarra.id) {
+                    guitarraState.cantidad = guitarra.cantidad;
+                }
+
+                return guitarraState;
+            });
+
+            setCarrito(carritoActualizado);
+        } else {
+            //* Registro nuevo
+            setCarrito([...carrito, guitarra]);
+        }
+    };
+
+    const eliminarProducto = (id) => {
+        const carritoActualizado = carrito.filter(
+            (producto) => producto.id != id
+        );
+        setCarrito(carritoActualizado);
+        window.localStorage.setItem("carrito", JSON.stringify(carrito));
+    };
+
+    const actualizarCantidad = (guitarra) => {
+        const carritoActualizado = carrito.map((guitarraState) => {
+            if (guitarraState.id === guitarra.id) {
+                guitarraState.cantidad = parseInt(guitarra.cantidad);
+            }
+            return guitarraState;
+        });
+        setCarrito(carritoActualizado);
+        window.localStorage.setItem("carrito", JSON.stringify(carrito));
+    };
+
+    return (
+        <Component
+            {...pageProps}
+            carrito={carrito}
+            agregarCarrito={agregarCarrito}
+            actualizarCantidad={actualizarCantidad}
+            eliminarProducto={eliminarProducto}
+        />
+    );
 }
